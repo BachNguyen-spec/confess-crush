e60ffe99f7282ca9ef606afb65c9d6dec26f3a0d
BachNguyen-spec <Bachsac03@gmail.com>
2025-02-14 17:48:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Tên người lạ từng quen",
+    introTitle: 'Trinh!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
